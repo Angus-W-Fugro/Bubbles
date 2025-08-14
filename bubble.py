866d4b42cb1057e44adb19aa1c747ea9edf6e68c
Angus-W-Fugro <a.warman@fugro.com>
2025-08-14 11:45:13 +0800

    wip

--- bubble.py
+++ bubble.py
@@ -5,8 +5,7 @@ def main():
     video_path = "C:/Users/a.warman/Downloads/vlc-record-2025-06-18-11h57m52s-12P_PYA_XNA_CON_MLC-10_2022-U_001_22-09-29_01-13-44_000.mp4-.mp4"
     cap = cv2.VideoCapture(video_path)
     stabilizer = VidStab()
-    # backSub = cv2.createBackgroundSubtractorKNN(dist2Threshold=2000)
-    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold=100, history=2000)
+    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold=128, history=500, detectShadows=False)
     
     if not cap.isOpened():
         print("Error: Could not open video.")
@@ -20,10 +19,13 @@ def main():
 
         stable_frame = stabilizer.stabilize_frame(input_frame=original_frame, border_size=50)
         foreground_frame = backSub.apply(stable_frame)
-        foreground_weighting = 0.5
-        background_weighting = 1
-        combined_frame = cv2.addWeighted(stable_frame, background_weighting, cv2.cvtColor(foreground_frame, cv2.COLOR_GRAY2BGR), foreground_weighting, 0)
-        cv2.imshow('Combined Frame', combined_frame)
+        cv2.imshow('Frame', foreground_frame)
+
+
+        # foreground_weighting = 1
+        # background_weighting = 0.1
+        # combined_frame = cv2.addWeighted(stable_frame, background_weighting, cv2.cvtColor(foreground_frame, cv2.COLOR_GRAY2BGR), foreground_weighting, 0)
+        # cv2.imshow('Combined Frame', combined_frame)
         
        # Break the loop if 'Esc' key is pressed
         if cv2.waitKey(1) == 27:
