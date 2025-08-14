961fd6a988567ae1011ea49be5b89a88cbd159c0
Angus-W-Fugro <a.warman@fugro.com>
2025-08-14 11:11:17 +0800

    wip

--- bubble.py
+++ bubble.py
@@ -1,9 +1,11 @@
 import cv2
 
+
+
 def main():
     video_path = "C:/Users/a.warman/Downloads/vlc-record-2025-06-18-11h57m52s-12P_PYA_XNA_CON_MLC-10_2022-U_001_22-09-29_01-13-44_000.mp4-.mp4"
     cap = cv2.VideoCapture(video_path)
-    backSub = cv2.createBackgroundSubtractorMOG2(varThreshold=100, detectShadows=False)
+    backSub = cv2.createBackgroundSubtractorKNN(dist2Threshold=2000)
     
     if not cap.isOpened():
         print("Error: Could not open video.")
@@ -23,8 +25,8 @@ def main():
         combined = cv2.addWeighted(frame, 1 - overlay_weighting, cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR), overlay_weighting, 0)
         cv2.imshow('Combined Frame', combined)
         
-        # Wait for 1 ms and check if 'q' is pressed to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+       # Break the loop if 'Esc' key is pressed
+        if cv2.waitKey(1) == 27:
             break
 
 if __name__ == "__main__":
