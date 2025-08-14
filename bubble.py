d875eb636a5456f17521a42c15fa36d9680bae3f
Angus-W-Fugro <a.warman@fugro.com>
2025-08-14 11:19:48 +0800

    stabilised?

--- bubble.py
+++ bubble.py
@@ -1,10 +1,10 @@
 import cv2
-
-
+from vidstab import VidStab
 
 def main():
     video_path = "C:/Users/a.warman/Downloads/vlc-record-2025-06-18-11h57m52s-12P_PYA_XNA_CON_MLC-10_2022-U_001_22-09-29_01-13-44_000.mp4-.mp4"
     cap = cv2.VideoCapture(video_path)
+    stabilizer = VidStab()
     backSub = cv2.createBackgroundSubtractorKNN(dist2Threshold=2000)
     
     if not cap.isOpened():
@@ -12,17 +12,17 @@ def main():
         return
     
     while True:
-        ret, frame = cap.read()
+        ret, original_frame = cap.read()
         if not ret:
             print("End of video or error reading frame.")
             break
 
-        fg_mask = backSub.apply(frame)
+        stabilised_frame = stabilizer.stabilize_frame(input_frame=original_frame, border_size=50)
+        fg_mask = backSub.apply(stabilised_frame)
         
         # Display the frame
-
         overlay_weighting = 0.8
-        combined = cv2.addWeighted(frame, 1 - overlay_weighting, cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR), overlay_weighting, 0)
+        combined = cv2.addWeighted(stabilised_frame, 1 - overlay_weighting, cv2.cvtColor(fg_mask, cv2.COLOR_GRAY2BGR), overlay_weighting, 0)
         cv2.imshow('Combined Frame', combined)
         
        # Break the loop if 'Esc' key is pressed
